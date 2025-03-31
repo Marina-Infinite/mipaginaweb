ee15e63475d21d2557f845480e5577fc0d0a9f4c
Marina-Infinite <marinaiclerc@gmail.com>
2025-03-31 15:14:20 -0300

    Update boton.js

--- boton.js
+++ boton.js
@@ -1,14 +1,38 @@
-// Selecciona todos los elementos con la clase .boton
 document.querySelectorAll('.boton').forEach(function(button) {
     button.addEventListener('click', function(event) {
-        // Puedes personalizar el mensaje según el modelo
+        // Obtiene el modelo
         let modelo = this.closest('.textoycarrito').querySelector('h3').innerText;
-        let message = `Hola, me interesa el producto ${modelo}`;
+        
+        // Obtiene el talle seleccionado
+        let talle = this.closest('.textoycarrito').querySelector('.talle-selector').value;
+        
+        // Obtiene la categoría desde el atributo data del body
+        let categoria = document.body.getAttribute('data-categoria');
+        
+        // Personaliza el mensaje
+        let message = `Hola, me interesa el producto ${modelo} en talle ${talle}, categoría ${categoria}`;
         let phone = '542241697504';
         
         handleLinkClick(event, `https://api.whatsapp.com/send?phone=${phone}&text=${encodeURIComponent(message)}`, `https://web.whatsapp.com/send?phone=${phone}&text=${encodeURIComponent(message)}`);
     });
 });
+
+document.querySelectorAll('.talle-selector').forEach(function(button) {
+    button.addEventListener('click', function() {
+      // Si el botón ya está seleccionado, lo deseleccionamos
+      if (this.classList.contains('activo')) {
+        this.classList.remove('activo');
+      } else {
+        // Primero, removemos la clase 'activo' de todos los botones
+        document.querySelectorAll('.talle-selector').forEach(function(btn) {
+          btn.classList.remove('activo');
+        });
+        // Luego, agregamos la clase 'activo' al botón clickeado
+        this.classList.add('activo');
+      }
+    });
+});
+  
 
 function handleLinkClick(event, appLink, webLink) {
     event.preventDefault();
@@ -25,7 +49,7 @@ document.querySelectorAll('.boton1').forEach(function(button) {
         
         // Personaliza el mensaje según el modelo
         let modelo = this.closest('.textoycarrito1').querySelector('h3').innerText;
-        let message = `Hola, me interesa el producto ${modelo}`;
+        let message = `Hola, me interesa el producto ${modelo} en talle ${talle}, categoría ${categoria}`;
         let phone = '2241697504'; // Asegúrate de que el número esté en el formato internacional sin el '+' inicial
 
         // Construir el enlace de WhatsApp con el mensaje
